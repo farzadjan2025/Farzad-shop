5ef92c9e167dd4788ec0d7080a81a0f275c332ef
farzadjan2025 <f.sarwari@yandex.com>
2025-07-28 23:42:47 +0430

    Update view_tmp_log.php

--- view_tmp_log.php
+++ view_tmp_log.php
@@ -1,8 +1,2 @@
 <?php
-$logFile = __DIR__ . '/ipn_log.txt';
-if (file_exists($logFile)) {
-    echo "<pre>" . htmlspecialchars(file_get_contents($logFile)) . "</pre>";
-} else {
-    echo "❌ فایل لاگ وجود ندارد.";
-}
-?>
+$path = '/tmp/ipn
