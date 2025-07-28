68a2411c3a0fc275a435459c2fe437013b0076cc
farzadjan2025 <f.sarwari@yandex.com>
2025-07-28 14:25:52 +0430

    Update ipn.php

--- ipn.php
+++ ipn.php
@@ -1,4 +1,3 @@
 <?php
-// فقط تست دریافت IPN
-file_put_contents(__DIR__ . "/ipn_log.txt", date("Y-m-d H:i:s") . " | IPN RECEIVED\n", FILE_APPEND);
-echo "✅ IPN RECEIVED";
+file_put_contents(__DIR__ . "/ipn_log.txt", date("Y-m-d H:i:s") . " | RAW: " . file_get_contents("php://input") . "\n", FILE_APPEND);
+echo "✅ RAW IPN Captured";
