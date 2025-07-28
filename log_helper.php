b049a78012f44f5e74752518a024dfc0aea86bd6
farzadjan2025 <f.sarwari@yandex.com>
2025-07-28 22:41:10 +0430

    Update log_helper.php

--- log_helper.php
+++ log_helper.php
@@ -1,1 +1,21 @@
+<?php
+// log_helper.php
 
+function log_debug($message, $data = null) {
+    $log_file = '/tmp/ipn_debug.txt'; // مسیر لاگ
+
+    $timestamp = date("Y-m-d H:i:s");
+    $entry = "[$timestamp] $message";
+
+    if ($data !== null) {
+        if (is_array($data) || is_object($data)) {
+            $entry .= "\n" . print_r($data, true);
+        } else {
+            $entry .= " => $data";
+        }
+    }
+
+    $entry .= "\n-------------------------\n";
+
+    file_put_contents($log_file, $entry, FILE_APPEND);
+}
