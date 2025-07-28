8043a1a2a463250cd7fc01c2f429bfa3623185dc
farzadjan2025 <f.sarwari@yandex.com>
2025-07-28 23:50:44 +0430

    Update view_ipn_debug.php

--- view_ipn_debug.php
+++ view_ipn_debug.php
@@ -1,1 +1,8 @@
-
+<?php
+$path = '/tmp/ipn_debug.txt';
+if (file_exists($path)) {
+    header('Content-Type: text/plain; charset=utf-8');
+    readfile($path);
+} else {
+    echo "فایل لاگ وجود ندارد.";
+}
