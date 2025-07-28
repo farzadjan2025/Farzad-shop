0e5f4aed423808e4c420ae105f08d6f7b6a88ecb
farzadjan2025 <f.sarwari@yandex.com>
2025-07-28 16:45:39 +0430

    Update view_log.php

--- view_log.php
+++ view_log.php
@@ -1,1 +1,11 @@
+<?php
+$log_file = __DIR__ . '/ipn_log.txt';
 
+if (!file_exists($log_file)) {
+    echo "❌ فایل لاگ وجود ندارد.";
+    exit;
+}
+
+$log = file_get_contents($log_file);
+echo "<pre>" . htmlspecialchars($log) . "</pre>";
+?>
